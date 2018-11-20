56e0c7a8c8faff46706a91390fdf60a80e324e5c
Johnny Kim <jxkim89@gmail.com>
2018-11-20 14:07:23 -0500

    207PM 11/20

--- src/data/UserDB.java
+++ src/data/UserDB.java
@@ -2,9 +2,30 @@ package data;
 
 import java.sql.*;
 
+import business.User;
+
 public class UserDB {
-	public static int  insert(User user) {
+	public static int insert(User user) {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement ps = null;
 		
+		String query
+				= "INSERT INTO User (Email, FirstName, LastName) "
+				+ " VALUES (? , ?, ?)";
+		try {
+			ps = connection.prepareStatement(query);
+			ps.setString(1, user.getName());
+			ps.setString(2, user.getEmail());
+			ps.setString(3, user.getEmploymentStatus());
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e);
+			return 0;
+		} finally {
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
 	}
 
 }
