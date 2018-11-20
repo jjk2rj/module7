5ae0004f929e0f39036ba15ee3e2ae32463c0b07
Johnny Kim <jxkim89@gmail.com>
2018-11-20 12:49:52 -0500

    3rd commit

--- src/data/ConnectionPool.java
+++ src/data/ConnectionPool.java
@@ -12,7 +12,34 @@ public class ConnectionPool {
 	private ConnectionPool() {
 		try {
 			InitialContext ic = new InitialContext();
-			dataSource = (DataSource) ic.lookup("java:)
+			dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/db");
+		} catch (NamingException e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	public static synchronized ConnectionPool getInstance() {
+		if(pool == null) {
+			pool = new ConnectionPool();
+		}
+		return pool;
+	}
+	
+	public Connection getConnection() {
+		try {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public void freeConnection(Connection c) {
+		try {
+			c.close();
+		} catch (SQLException e) {
+			System.out.println(e);
 		}
 	}
 	
